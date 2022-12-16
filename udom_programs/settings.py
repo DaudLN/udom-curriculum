cfc23df2369c76125b27a12c41be094062b9bac2
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2022-12-16 18:52:34 +0300

    Update settings.py

--- udom_programs/settings.py
+++ udom_programs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-837es7d5wpn&e4nch!mv0_wj7g9ytwz90qiayq^f-7$j!m3p4+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
