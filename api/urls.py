535d137a2b82d15e3550f75f55e76f100a992fa0
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2022-12-16 18:57:29 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProgramListAPIView, ProgramRetrieveAPIView
 urlpatterns = [
     path('?format=json', ProgramListAPIView.as_view()),
-    path('<int:pk>', ProgramRetrieveAPIView.as_view())
+    path('?format=json/<int:pk>', ProgramRetrieveAPIView.as_view())
 ]
