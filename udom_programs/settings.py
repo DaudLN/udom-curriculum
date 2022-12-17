af7276b630d8aa7d216524afb708dbd116e9ba41
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2022-12-17 17:58:42 +0300

    Update settings.py

--- udom_programs/settings.py
+++ udom_programs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-837es7d5wpn&e4nch!mv0_wj7g9ytwz90qiayq^f-7$j!m3p4+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['udom-curriculum.up.railway.app']
 
 # Application definition
 
