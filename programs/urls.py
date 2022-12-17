9a146052f5a10b10a66cb6ba490c0b7e67419db9
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2022-12-17 15:41:40 +0300

    Update urls.py

--- programs/urls.py
+++ programs/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('pdf', ProgramDetailView.generate_pdf, name='generate_pdf'),
     path('programs/', ProgramListView.as_view(), name='program_list'),
-    path('programs/<int:pk>', ProgramDetailView.as_view(), name='program_detail'),
+    path('programs/<int:pk>',
+         ProgramDetailView.as_view(),
+         name='program_detail'),
+    path('searh_program/', search_program, name="search_program"),
+    path('generate_pdf/<int:pk>', generate_pdf, name="program_pdf"),
 ]
