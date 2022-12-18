4d265a63b329749b0729a53513946e7594fee0fe
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2022-12-18 20:18:44 +0300

    Update models.py

--- programs/models.py
+++ programs/models.py
@@ -1,14 +1,20 @@
 from django.db import models
+from django.urls import reverse
 from .colleges import *
 from .programs import *
 
 
 class Program(models.Model):
 
-    name = models.CharField(max_length=255, null=False,
-                            blank=False, unique=True)
-    college = models.CharField(max_length=255, null=False, blank=False,
-                               choices=college_choices, default=CIVE)
+    name = models.CharField(max_length=255,
+                            null=False,
+                            blank=False,
+                            unique=True)
+    college = models.CharField(max_length=255,
+                               null=False,
+                               blank=False,
+                               choices=college_choices,
+                               default=CHAS)
     description = models.TextField(null=True, blank=True)
     years_of_study = models.SmallIntegerField()
     fee = models.PositiveIntegerField()
@@ -17,9 +23,13 @@ class Program(models.Model):
     competences = models.TextField(null=True, blank=True)
     special_requirements = models.TextField(null=True, blank=True)
     fields_of_work = models.TextField()
+    slug = models.SlugField(max_length=255, null=True, blank=True)
 
     class Meta:
         ordering = ['name']
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('program_detail', kwargs={'slug': self.slug})
