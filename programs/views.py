f09a957a2f82507a5595359a79bf7d69e94e8bd6
DaudLN <daudnamayala@gmail.com>
2023-03-06 22:41:10 +0300

    More feature to search

--- programs/views.py
+++ programs/views.py
@@ -1,4 +1,5 @@
 import io
+from django.db import Q
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
@@ -32,82 +33,14 @@ def search_program(request):
 
     if request.method == "POST":
         search = request.POST["search"]
-        programs = Program.objects.filter(name__icontains=search)
+        programs = Program.objects.filter(
+            Q(name__icontains=search) | Q(college__icontains=search)
+        )
 
         return JsonResponse({"programs": list(programs.values())})
-        # return render(request, "program_search.html", {"programs": programs})
+
     else:
         return render(request, "program_search.html", {})
-
-
-# def generate_pdf(request, program):
-#     # Get the Program object
-#     program = get_object_or_404(Program, slug=program)
-
-#     # Create the PDF object
-#     response = HttpResponse(content_type="application/pdf")
-#     response["Content-Disposition"] = f'attachment; filename="{program.name}.pdf"'
-#     buffer = io.BytesIO()
-#     doc = SimpleDocTemplate(buffer, pagesize=letter)
-#     styles = getSampleStyleSheet()
-
-#     # Create a list of paragraphs to draw on the PDF
-#     elements = []
-#     university_head = "<a href='https://www.udom.ac.tz'>THE UNIVERSITY OF DODOMA </a>"
-#     university_head_style = getSampleStyleSheet()["Heading1"]
-#     university_head_style.alignment = enums.TA_CENTER
-#     college_head = f"{program.college}"
-#     college_head_style = getSampleStyleSheet()["Heading2"]
-#     college_head_style.alignment = enums.TA_CENTER
-#     paragrap_style = getSampleStyleSheet()["Normal"]
-#     paragrap_style.fontSize = 11
-#     elements.append(Paragraph(university_head, style=university_head_style))
-#     elements.append(Paragraph(college_head, style=college_head_style))
-#     elements.append(
-#         Image(
-#             "https://imgs.search.brave.com/md-s9-5eWujI9NxzCGJxGDSugTfBhqJvsUdta4Xk5kc/rs:fit:319:320:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzYxLzc0/L2Y4LzYxNzRmOGVi/MzQ3MDlmNGQwNWRm/ZWU3NTZiYzA5MTkz/LnBuZw",
-#             width=100,
-#             height=100,
-#         )
-#     )
-#     elements.append(
-#         Paragraph(
-#             f"The University of Dodoma Program curriculum", style=college_head_style
-#         )
-#     )
-#     elements.append(Paragraph(f"{program.name}", style=college_head_style))
-#     elements.append(Paragraph(f"Description", styles["Heading3"]))
-#     elements.append(Paragraph(f"{program.description}", style=paragrap_style))
-#     elements.append(Paragraph(f"Program tuition fee", styles["Heading3"]))
-#     elements.append(Paragraph(f"Tsh {program.fee}", style=paragrap_style))
-#     elements.append(Paragraph(f"College", styles["Heading3"]))
-#     elements.append(Paragraph(f"{program.college}", style=paragrap_style))
-#     if program.knowledge:
-#         elements.append(Paragraph("Knowledge obtained", styles["Heading3"]))
-
-#         elements.append(Paragraph(f"{program.knowledge}", style=paragrap_style))
-#     else:
-#         pass
-
-#     if program.skills:
-#         elements.append(Paragraph("Skills", styles["Heading3"]))
-#         elements.append(Paragraph(f"{program.skills}", style=paragrap_style))
-#     else:
-#         pass
-#     elements.append(Paragraph("Competences", styles["Heading3"]))
-#     elements.append(Paragraph(f"{program.competences}", style=paragrap_style))
-#     elements.append(Paragraph("Special requirements", styles["Heading3"]))
-#     elements.append(Paragraph(f"{program.special_requirements}", style=paragrap_style))
-#     elements.append(Paragraph("Field of work", styles["Heading3"]))
-#     elements.append(Paragraph(f"{program.fields_of_work}", style=paragrap_style))
-
-#     # Draw the PDF
-#     doc.build(elements)
-
-#     # Get the value of the BytesIO buffer and write it to the response.
-#     response.write(buffer.getvalue())
-#     buffer.close()
-#     return response
 
 
 def generate_pdf(request, program):
