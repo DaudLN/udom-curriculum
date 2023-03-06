a19816ff23ae047a22fec26af8948e1ee8664c5a
DaudLN <daudnamayala@gmail.com>
2023-03-06 20:52:59 +0300

    Allow to generate pdf

--- programs/urls.py
+++ programs/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("programs", ProgramListView.as_view(), name="program_list"),
     path("programs/<slug:program>", program_detail, name="program_detail"),
     path("searh_program", search_program, name="search_program"),
-    path("program/generate_pdf/<slug:program>", generate_pdf, name="program_pdf"),
+    path("programs/generate_pdf/<slug:program>", generate_pdf, name="program_pdf"),
     # path("colleges/", list_of_college, name="list_of_college")
 ]
