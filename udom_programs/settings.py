b00b26e749498cf44ba1fec8ee40eae879c89cac
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2023-03-07 02:12:01 +0300

    Update settings.py

--- udom_programs/settings.py
+++ udom_programs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-837es7d5wpn&e4nch!mv0_wj7g9ytwz90qiayq^f-7$j!m3p4+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["udomecurriculum.up.railway.app", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ["https://udomecurriculum.up.railway.app"]
